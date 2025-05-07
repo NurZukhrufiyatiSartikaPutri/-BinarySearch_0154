32851c158f89afb66b30b9c927f7dd537cc292aa
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-07 14:44:51 +0700

    perintah memulai perulangan dari indeks 0 sampai sebelum nPanjang

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -54,5 +54,6 @@ void display()
     cout << "\n============================================\n";
     cout << "       Elemen Array Setelah Diurutkan (Asc)   \n";
     cout << "==============================================\n";
-    
+    for (int j = 0; j < nPanjang; j++)
+   
 }
