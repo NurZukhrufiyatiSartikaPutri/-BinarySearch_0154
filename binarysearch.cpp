113ba1f80c8dd47eae01b4e9732c293594852485
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-07 13:47:14 +0700

    perintah untuk menampilkan element maksimal 10

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -2,3 +2,13 @@
 using namespace std;
 
 int element[10];
+int nPanjnag;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+    }
+}
